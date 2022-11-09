93bdf34d23c1c94811ded1320281e2f10c48a5bb
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-11-10 03:19:29 +0530

    Added : Player Rolls the Die

---
+++ UC2_Snake_And_Ladder.java
@@ -0,0 +1,13 @@
+package Snake_And_Ladder;
+
+public class UC2_Snake_And_Ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start The Game");
+		int dicecheck = (int) (Math.random() * 6) + 1;
+		System.out.println("Dice thrown no is " + dicecheck);
+
+	}
+
+}
