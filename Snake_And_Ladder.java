b66f2c2893387e15b724bc46f70d883346e2d53a
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-11-10 03:07:44 +0530

    Added : Main File

---
+++ Snake_And_Ladder.java
@@ -0,0 +1,9 @@
+package Snake_And_Ladder;
+
+public class Snake_And_Ladder {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome in Snake & Ladder Game");
+}
+
+}
