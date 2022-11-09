d4024b51fb8be3f840668a527f6bd619f282b35d
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-11-10 03:14:59 +0530

    Added : Player at Start Position 0

---
+++ UC1_Snake_And_Ladder.java
@@ -0,0 +1,13 @@
+package Snake_And_Ladder;
+
+public class UC1_Snake_And_Ladder {
+	static int player = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake Ladder Program");
+		System.out.println("Player is at position : " + player);
+
+	}
+
+}
